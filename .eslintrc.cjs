39b91e05fb6dadca68e10ddb3a6916d5729a8eb9
Dmitry Ivanko <tmwsls12@gmail.com>
2023-01-10 18:35:17 +0300

    Init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,89 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    es2022: true,
+    browser: true,
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  rules: {
+    'eol-last': ['warn', 'always'],
+    'block-spacing': 'warn',
+    indent: [
+      'warn',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    'no-multiple-empty-lines': [
+      'warn',
+      {
+        max: 1,
+        maxBOF: 0,
+        maxEOF: 0,
+      },
+    ],
+    'import/extensions': 'off',
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'object-curly-spacing': ['warn', 'never'],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    semi: ['error', 'never'],
+    'semi-spacing': ['warn'],
+    'comma-dangle': [2, 'always-multiline'],
+    'comma-style': ['warn'],
+    'no-unused-vars': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        ignorePattern: "className='.*'",
+      },
+    ],
+    '@typescript-eslint/no-non-null-assertion': ['off'],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-multi-spaces': 'warn',
+    'simple-import-sort/imports': 'warn',
+  },
+  settings: {
+    'svelte3/typescript': require('typescript'),
+    // ignore style tags in Svelte because of Tailwind CSS
+    // See https://github.com/sveltejs/eslint-plugin-svelte3/issues/70
+    'svelte3/ignore-styles': () => true,
+  },
+  plugins: ['svelte3', '@typescript-eslint', 'simple-import-sort'],
+  ignorePatterns: ['node_modules', 'svelte.config.js'],
+}
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # tit-web-client
+
+## Dependencies
+
+### Mandatory
+
+- Node.js version 16 and up. Recommended use [Node Version Manager](https://github.com/nvm-sh/nvm) for installation.
+
+### Optional
+
+- To manage client dependencies it is recommended to use [Yarn](https://yarnpkg.com). In order to install it, you need to run:
+
+```bash
+$ npm install --global yarn
+```
+
+## Getting started
+
+### Development
+
+You need to install dependencies and run the client
+
+```bash
+$ yarn install
+$ yarn dev
+```
