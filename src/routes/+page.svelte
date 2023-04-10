e08cc3baea18b622b76df51b6fa62d99339bfe9a
Eduard Stromenko <estromenko@mail.ru>
2023-04-10 18:33:22 +0300

    Add landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <script>
-  import {Alert} from 'flowbite-svelte'
+  import {fade} from 'svelte/transition'
+
+  import ArrowIcon from '$lib/components/icons/ArrowIcon/ArrowIcon.svelte'
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div class="p-8">
-  <Alert>
-    <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
-  </Alert>
-</div>
+<section
+  transition:fade
+  class="bg-center fixed h-screen w-screen bg-cover bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply"
+>
+  <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+    <h1
+      class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl"
+    >
+      TutorinTech
+    </h1>
+    <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+      Next generation CybSec education platform
+    </p>
+    <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+      <a
+        href="/dashboard"
+        class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+      >
+        Get started
+        <ArrowIcon />
+      </a>
+    </div>
+  </div>
+</section>
