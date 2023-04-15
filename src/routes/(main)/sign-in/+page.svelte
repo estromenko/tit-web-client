3c0a49adaa2aef6643e13f916bf6515849750592
Eduard Stromenko <estromenko@mail.ru>
2023-04-15 14:22:53 +0300

    Replace sign-in and sign-out images

--- src/routes/(main)/sign-in/+page.svelte
+++ src/routes/(main)/sign-in/+page.svelte
@@ -10,7 +10,7 @@
   <title>TiT — Sign In</title>
 </svelte:head>
 
-<section class="h-90v">
+<section class="h-[calc(100vh-100px)]">
   <div class="px-6 h-full text-gray-800">
     <div
       class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
@@ -18,7 +18,7 @@
       <div
         class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
       >
-        <img src="https://i.ytimg.com/vi/DN-1AAVsTJ0/hqdefault.jpg" class="w-full" alt="Sign in" />
+        <img src="/logo.png" class="h-full m-auto" alt="Sign in" />
       </div>
       <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
         <SignInForm actionData={form} />
